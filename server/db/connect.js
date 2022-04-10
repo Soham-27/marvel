39e561e49c6c84346c189d5be5f3569ca0e8d073
KedarKK1 <officialkedark1@gmail.com>
2022-04-10 14:39:04 +0530

    tables added

---
+++ server/db/connect.js
@@ -0,0 +1,26 @@
+const { Client } = require("pg");
+const fs = require("fs");
+
+const configDev = {
+  host: process.env.POSTGRES_HOST,
+  user: process.env.POSTGRES_USER,
+  port: process.env.POSTGRES_PORT,
+  // connectionTimeoutMillis: 2000,
+  password: process.env.POSTGRES_PASSWORD,
+  database: "submission",
+};
+const configProd = {
+   connectionString: process.env.CONN_STRING,
+   ssl: {
+     rejectUnauthorized: false,
+     ca: fs.readFileSync('ca-certificate.crt').toString(),
+   },
+};
+
+// const configProd = {};
+
+const client = new Client(
+  process.env.NODE_ENV === "dev" ? configDev : configProd
+);
+
+module.exports = client;
---
+++ server/db/init.sql
@@ -0,0 +1,76 @@
+CREATE DATABASE submission;
+
+\c submission;
+
+CREATE TABLE users (
+	id INT GENERATED ALWAYS AS IDENTITY UNIQUE,
+	first_name VARCHAR(255) NOT NULL,
+	surname VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	created_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE user_token (
+	id INT GENERATED ALWAYS AS IDENTITY UNIQUE,
+	token VARCHAR(255) NOT NULL,
+	is_valid BOOLEAN NOT NULL,
+	fk_user INT NOT NULL REFERENCES users(id) 
+);
+
+
+CREATE TABLE event_details (
+	id INT GENERATED ALWAYS AS IDENTITY UNIQUE,
+	event_name VARCHAR(255) NOT NULL,
+	ems_event_id INT,
+    submission_table VARCHAR(255) NOT NULL,
+	start_time TIMESTAMP NOT NULL,
+	end_time TIMESTAMP NOT NULL, 
+	created_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE webapp_submission (
+    id INT GENERATED ALWAYS AS IDENTITY UNIQUE,
+    fk_user INT NOT NULL REFERENCES users(id),
+	submission VARCHAR(256) NOT NULL,
+    active_submission BOOLEAN NOT NULL,
+	created_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE paper_submission (
+    id INT GENERATED ALWAYS AS IDENTITY UNIQUE,
+    fk_user INT NOT NULL REFERENCES users(id),
+	submission VARCHAR(255) NOT NULL,
+    active_submission BOOLEAN NOT NULL,
+	created_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE photoshop_submission (
+    id INT GENERATED ALWAYS AS IDENTITY UNIQUE,
+    fk_user INT NOT NULL REFERENCES users(id),
+	submission VARCHAR(255) NOT NULL,
+    active_submission BOOLEAN NOT NULL,
+	created_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE insight_submission (
+    id INT GENERATED ALWAYS AS IDENTITY UNIQUE,
+    fk_user INT NOT NULL REFERENCES users(id),
+	submission VARCHAR(255) NOT NULL,
+    active_submission BOOLEAN NOT NULL,
+	created_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE dataquest_submission (
+    id INT GENERATED ALWAYS AS IDENTITY UNIQUE,
+    fk_user INT NOT NULL REFERENCES users(id),
+	submission VARCHAR(255) NOT NULL,
+    active_submission BOOLEAN NOT NULL, 
+	created_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP NOT NULL
+);
