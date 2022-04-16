df5793d7fccd8e8c3093219e4fd6c0b3066bad1b
= <=>
2022-04-16 12:43:58 +0530

    Going Live

--- server/models/master.js
+++ server/models/master.js
@@ -1,37 +1,37 @@
 const masterEvents = [
     {
         id: 1,
-        ems_id: 3,
+        ems_id: 8,
         name: 'Web N App',
     },
     {
         id: 2,
-        ems_id: 4,
+        ems_id: 13,
         name: 'Insight',
     },
     {
         id: 3,
-        ems_id: 5,
+        ems_id: 14,
         name: 'Photoshop Royale',
     },
     {
         id: 4,
-        ems_id: 2,
+        ems_id: 9,
         name: 'DataQuest',
     },
     {
         id: 5,
-        ems_id: 6,
+        ems_id: 10,
         name: 'Paper Presentation',
     },
 ]
 
 const emsIds = {
-    dataquest: 2,
-    web: 3,
-    insight: 4,
-    photoshop: 5,
-    paper: 6,
+    dataquest: 9,
+    web: 8,
+    insight: 13,
+    photoshop: 14,
+    paper: 10,
 }
 
 module.exports = {masterEvents, emsIds};
