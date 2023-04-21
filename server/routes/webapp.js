baa683e411dcd8c1faad6b947f1cc29257e7efd3
PRATHAM MORE <pratham02more20@gmail.com>
2023-04-21 08:22:19 +0530

    errors resolved

--- server/routes/webapp.js
+++ server/routes/webapp.js
@@ -1,74 +1,69 @@
 // const axios = require('axios');
 
-const express = require('express');
-const client = require('../db/connect');
-const isUserAuthenticated = require('../middleware/userMiddleware');
-const {emsIds} = require("../models/master");
+const express = require("express");
+const client = require("../db/connect");
+const isUserAuthenticated = require("../middleware/userMiddleware");
+const { emsIds } = require("../models/master");
 
 const router = new express.Router();
 
-router.post('/',isUserAuthenticated, async(req,res)=>{
-    const { submission } = req.body;
-    const ems_id= emsIds.web;
-    try {
-        const data= await client.query(
-            "SELECT* FROM user_events WHERE fk_user=$1 and ems_id=$2",
-            [req.user.id, ems_id]
-        );
+router.post("/", isUserAuthenticated, async (req, res) => {
+  const { submission } = req.body;
+  const ems_id = emsIds.web;
+  try {
+    const data = await client.query(
+      "SELECT * FROM user_events WHERE fk_user=$1 and ems_id=$2",
+      [req.user.id, ems_id]
+    );
 
-        if(data.rowCount===0){
-           return res.status(400).send({
-               error:"User havent Registered "
-           })
-        }
-        const query = `select exists (select * from webapp where fk_user = $1)`
-        const result = await client.query(query, [req.user.id]);
-        if(result.rows[0].exists) {
-            return res.status(400).send({
-                error: "Entry Already Submitted",
-            })
-        }
-        const response= await client.query(
-            "INSERT INTO webapp(submission, active_submission, fk_user, created_at, updated_at) VALUES($1, $2, $3, $4, $5) RETURNING *",
-            [submission, true, req.user.id,new Date(), new Date() ]
-        );
-        res.send({
-            submission:response.rows[0]
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({
-           error: "Internal Server error"
-          });
-        
+    if (data.rowCount === 0) {
+      return res.status(400).send({
+        error: "User havent Registered ",
+      });
     }
-})
-
-router.get('/', isUserAuthenticated, async (req, res) => {
-    try {
-        const response = await client.query(
-            "SELECT * FROM webapp WHERE fk_user= $1",
-            [req.user.id]
-        );
-
-        if (response.rowCount === 0) {
-            return res.status(400).send({
-                error: "Nothing Submitted"
-            })
-        }
-        res.send({
-            submission: response.rows[0]
-        })
-
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({
-            error: "Internal Server error"
-        });
-
+    const query = `select exists (select * from webapp where fk_user = $1)`;
+    const result = await client.query(query, [req.user.id]);
+    if (result.rows[0].exists) {
+      return res.status(400).send({
+        error: "Entry Already Submitted",
+      });
     }
-})
+    const response = await client.query(
+      "INSERT INTO webapp(submission, active_submission, fk_user, created_at, updated_at) VALUES($1, $2, $3, $4, $5) RETURNING *",
+      [submission, true, req.user.id, new Date(), new Date()]
+    );
+    res.send({
+      submission: response.rows[0],
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      error: "Internal Server error",
+    });
+  }
+});
 
+router.get("/", isUserAuthenticated, async (req, res) => {
+  try {
+    const response = await client.query(
+      "SELECT * FROM webapp WHERE fk_user= $1",
+      [req.user.id]
+    );
+
+    if (response.rowCount === 0) {
+      return res.status(400).send({
+        error: "Nothing Submitted",
+      });
+    }
+    res.send({
+      submission: response.rows[0],
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      error: "Internal Server error",
+    });
+  }
+});
 
 module.exports = router;
--- server/utils/fetchEvents.js
+++ server/utils/fetchEvents.js
@@ -1,61 +1,71 @@
 const { default: axios } = require("axios");
 const client = require("../db/connect");
-const { masterEvents } =  require("../models/master");
-const format = require('pg-format');
+const { masterEvents } = require("../models/master");
+const format = require("pg-format");
 
 const setArr = (ems_events, submission_events, ems_id, userId, finalArr) => {
-    const ems_event = ems_events?.find((event)=>event.id === ems_id);
-    if(!ems_event) {
-        return finalArr;
-    }
-    if(submission_events?.find((event)=>event.ems_id === ems_id)) {
-        return finalArr;
-    }
-    const arr = [
-        ems_event.id,
-        ems_event.name,
-        ems_event.description,
-        ems_event.type,
-        ems_event.mode,
-        ems_event.is_active,
-        ems_event.play,
-        ems_event.tagline,
-        ems_event.logo,
-        ems_event.start_time,
-        ems_event.end_time,
-        userId,
-        new Date(),
-        new Date()
-    ]
-    finalArr.push(arr);
+  const ems_event = ems_events?.find((event) => event.id === ems_id);
+  if (!ems_event) {
     return finalArr;
-}
+  }
+  if (submission_events?.find((event) => event.ems_id === ems_id)) {
+    return finalArr;
+  }
+  const arr = [
+    ems_event.id,
+    ems_event.name,
+    ems_event.description,
+    ems_event.type,
+    ems_event.mode,
+    ems_event.is_active,
+    ems_event.play,
+    ems_event.tagline,
+    ems_event.logo,
+    ems_event.start_time,
+    ems_event.end_time,
+    userId,
+    new Date(),
+    new Date(),
+  ];
+  finalArr.push(arr);
+  return finalArr;
+};
 
 const fetchEvents = async (ems_token, userId) => {
-    const options = {
-        method: "GET",
-        url: `${process.env.EMS_API}/user_events`,
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${ems_token}`,
-        },
-    };
-    try {
-        const response = await axios(options);
-        const result = await client.query(`select * from user_events where fk_user = $1`, [userId]);
-        let ems_events = await response.data.events
-        const submission_events = await result.rows;
-        let finalArr = [];
-        masterEvents.forEach((event)=> {
-            finalArr = setArr(ems_events, submission_events, event.ems_id, userId, finalArr);
-        });
-        if(finalArr?.length > 0) {
-            const query = `INSERT into user_events (ems_id, name, description, type, mode, is_active, play, tagline, logo, start_time, end_time, fk_user, created_at, updated_at) VALUES %L`
-            await client.query(format(query, finalArr));
-        }
-    } catch(err) {
-        console.log(err);
+  const options = {
+    method: "GET",
+    url: `${process.env.EMS_API}/user_events`,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${ems_token}`,
+    },
+  };
+  try {
+    const response = await axios(options);
+    console.log("User Events fetched from EMS", response);
+    const result = await client.query(
+      `select * from user_events where fk_user = $1`,
+      [userId]
+    );
+    let ems_events = response.data.events;
+    const submission_events = await result.rows;
+    let finalArr = [];
+    masterEvents.forEach((event) => {
+      finalArr = setArr(
+        ems_events,
+        submission_events,
+        event.ems_id,
+        userId,
+        finalArr
+      );
+    });
+    if (finalArr?.length > 0) {
+      const query = `INSERT into user_events (ems_id, name, description, type, mode, is_active, play, tagline, logo, start_time, end_time, fk_user, created_at, updated_at) VALUES %L`;
+      await client.query(format(query, finalArr));
     }
-}
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 module.exports = fetchEvents;
