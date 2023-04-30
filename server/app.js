751ad1b1c084fb5cea9e22625218a44dfcac2be4
PRATHAM MORE <pratham02more20@gmail.com>
2023-05-01 00:49:51 +0530

    errors resolved

--- server/app.js
+++ server/app.js
@@ -1,21 +1,20 @@
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
+let express = require("express");
+let path = require("path");
+let cookieParser = require("cookie-parser");
+let logger = require("morgan");
 let client = require("./db/connect");
-var cors = require('cors')
+var cors = require("cors");
 
-
-
-let indexRouter = require('./routes/index');
+let indexRouter = require("./routes/index");
 const userRouter = require("./routes/user");
 const userEventsRouter = require("./routes/user_events");
-const webappRouter= require("./routes/webapp");
-const paperRouter= require("./routes/paperpresentation");
-const photoshopRouter= require("./routes/photoshop");
-const insightRouter= require("./routes/insight");
+const webappRouter = require("./routes/webapp");
+const paperRouter = require("./routes/paperpresentation");
+const photoshopRouter = require("./routes/photoshop");
+const insightRouter = require("./routes/insight");
 const dataquestRouter = require("./routes/dataquest");
 const dataquest2Router = require("./routes/dataquest2");
+const frrezeRouter = require("../routes/freeze");
 // const adminRouter= require("./routes/adminRoutes");
 
 let app = express();
@@ -24,24 +23,25 @@ client
   .connect()
   .then(() => console.log("connected"))
   .catch((err) => console.error("connection error", err.stack));
-  
-app.use(cors()) 
-app.use(logger('dev'));
+
+app.use(cors());
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/user",userRouter);
+app.use("/user", userRouter);
 app.use("/user_events", userEventsRouter);
 app.use("/webapp", webappRouter);
 app.use("/paper", paperRouter);
 app.use("/photoshop", photoshopRouter);
-app.use("/insight",insightRouter);
-app.use('/dataquest', dataquestRouter);
-app.use('/dataquest2', dataquest2Router);
+app.use("/insight", insightRouter);
+app.use("/dataquest", dataquestRouter);
+app.use("/dataquest2", dataquest2Router);
+app.use("/freeze", frrezeRouter);
 // app.use("/admin", adminRouter);
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
 module.exports = app;
---
+++ server/routes/freeze.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const client = require('../db/connect');
+const isUserAuthenticated = require('../middleware/userMiddleware');
+const { emsIds } = require("../models/master");
+
+const router = express.Router();
+
+router.post('/', isUserAuthenticated, async (req, res) => {
+    const { submission } = req.body;
+    const ems_id = emsIds.freeze;
+    try {
+
+        const data = await client.query(
+            "SELECT * FROM user_events WHERE fk_user=$1 and ems_id=$2",
+            [req.user.id, ems_id]
+        );
+
+        if (data.rowCount === 0) {
+            return res.status(400).send({
+                error: "User havent Registered "
+            })
+        }
+
+        const query = `select exists (select * from freeze where fk_user = $1)`
+        const result = await client.query(query, [req.user.id]);
+        if(result.rows[0].exists) {
+            return res.status(400).send({
+                error: "Entry Already Submitted",
+            })
+        }
+
+        const response = await client.query(
+            "INSERT INTO freeze(submission, active_submission, fk_user, created_at, updated_at) VALUES($1, $2, $3, $4, $5) RETURNING * ",
+            [submission, true, req.user.id, new Date(), new Date()]
+        );
+        res.send({
+            submission: response.rows[0]
+        })
+
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            error: "Internal Server error"
+        });
+
+
+    }
+})
+
+router.get('/', isUserAuthenticated, async (req, res) => {
+    try {
+        const response = await client.query(
+            "SELECT * FROM freeze WHERE fk_user= $1",
+            [req.user.id]
+        );
+
+        if (response.rowCount === 0) {
+            return res.status(400).send({
+                error: "Nothing Submitted"
+            })
+        }
+        res.send({
+            submission: response.rows[0]
+        })
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            error: "Internal Server error"
+        });
+
+    }
+})
+
+module.exports = router;
