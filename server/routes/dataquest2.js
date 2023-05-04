0dc1ea0d819ef8e910921a370b63fac6225b7db0
PRATHAM MORE <pratham02more20@gmail.com>
2023-05-04 11:09:02 +0530

    leaderboard changes

--- server/routes/dataquest2.js
+++ server/routes/dataquest2.js
@@ -28,24 +28,24 @@ router.post("/", isUserAuthenticated, async (req, res) => {
     }
     const query = `select * from dataquest2 where fk_user = $1 and Date(created_at) = $2`;
     const result = await client.query(query, [req.user.id, currDate]);
-    if (result.rowCount >= 3) {
+    if (result.rowCount >= 8) {
       return res.status(400).send({
         error: "You have reached maximum submission limit for today.",
       });
     }
     let options;
-    
-      options = {
-        method: "POST",
-        url: `${process.env.EVALUATION}/te-be-round-2`,
-        headers: {
-          "Content-Type": "application/json",
-        },
-        data: {
-          file_url: submission_csv,
-        },
-      };
-    
+
+    options = {
+      method: "POST",
+      url: `${process.env.EVALUATION}/te-be-round-2`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: {
+        file_url: submission_csv,
+      },
+    };
+
     console.log("No error at line 48");
     const accData = await axios(options);
     console.log("No error at line 50");
@@ -98,36 +98,35 @@ router.get("/", isUserAuthenticated, async (req, res) => {
 });
 
 router.get("/leaderboard", isUserAuthenticated, async (req, res) => {
-  const seniors = ["TE", "BE"];
+  const seniors = ["FE", "SE", "TE", "BE"];
   const isSenior = seniors.includes(req.user.year) ? seniors : ["FE", "SE"];
   try {
-    if (seniors.includes(req.user.year)) {
-      const response = await client.query(
-        "select  users.first_name, users.last_name, MAX(dataquest2.public_accuracy), users.email from dataquest2 join users on dataquest2.fk_user = users.id where users.year = $1 or users.year = $2 group by users.first_name, users.last_name, users.email order by 3 DESC limit 3",
-        isSenior
-      );
-      if (response.rowCount === 0) {
-        return res.status(400).send({
-          error: "Leaderboard is empty",
-        });
-      }
-      res.send({
-        submissions: response.rows,
-      });
-    } else {
-      const response = await client.query(
-        "select  users.first_name, users.last_name, MIN(dataquest2.public_accuracy), users.email from dataquest2 join users on dataquest2.fk_user = users.id where users.year = $1 or users.year = $2 group by users.first_name, users.last_name, users.email order by 3 limit 3",
-        isSenior
-      );
-      if (response.rowCount === 0) {
-        return res.status(400).send({
-          error: "Leaderboard is empty",
-        });
-      }
-      res.send({
-        submissions: response.rows,
+    // if (seniors.includes(req.user.year)) {
+    //   const response = await client.query(
+    //     "select  users.first_name, users.last_name, MAX(dataquest2.public_accuracy), users.email from dataquest2 join users on dataquest2.fk_user = users.id where users.year = $1 or users.year = $2 group by users.first_name, users.last_name, users.email order by 3 DESC limit 3",
+    //     isSenior
+    //   );
+    //   if (response.rowCount === 0) {
+    //     return res.status(400).send({
+    //       error: "Leaderboard is empty",
+    //     });
+    //   }
+    //   res.send({
+    //     submissions: response.rows,
+    //   });
+    // } else {
+    const response = await client.query(
+      "select  users.first_name, users.last_name, MIN(dataquest2.public_accuracy), users.email from dataquest2 join users on dataquest2.fk_user = users.id where users.year = $1 or users.year = $2 group by users.first_name, users.last_name, users.email order by 3 limit 3",
+      isSenior
+    );
+    if (response.rowCount === 0) {
+      return res.status(400).send({
+        error: "Leaderboard is empty",
       });
     }
+    res.send({
+      submissions: response.rows,
+    });
   } catch (error) {
     console.log(error);
     res.status(500).send({
