7cc44498c8712bf7bb84497c264caf14dd256ffb
PRATHAM MORE <pratham02more20@gmail.com>
2023-05-04 11:20:37 +0530

    errors resolved

--- server/routes/dataquest2.js
+++ server/routes/dataquest2.js
@@ -101,32 +101,33 @@ router.get("/leaderboard", isUserAuthenticated, async (req, res) => {
   const seniors = ["FE", "SE", "TE", "BE"];
   const isSenior = seniors.includes(req.user.year) ? seniors : ["FE", "SE"];
   try {
-    // if (seniors.includes(req.user.year)) {
-    //   const response = await client.query(
-    //     "select  users.first_name, users.last_name, MAX(dataquest2.public_accuracy), users.email from dataquest2 join users on dataquest2.fk_user = users.id where users.year = $1 or users.year = $2 group by users.first_name, users.last_name, users.email order by 3 DESC limit 3",
-    //     isSenior
-    //   );
-    //   if (response.rowCount === 0) {
-    //     return res.status(400).send({
-    //       error: "Leaderboard is empty",
-    //     });
-    //   }
-    //   res.send({
-    //     submissions: response.rows,
-    //   });
-    // } else {
-    const response = await client.query(
-      "select  users.first_name, users.last_name, MIN(dataquest2.public_accuracy), users.email from dataquest2 join users on dataquest2.fk_user = users.id where users.year = $1 or users.year = $2 or users.year = $3 or users.year = $4 group by users.first_name, users.last_name, users.email order by 3 limit 3",
-      isSenior
-    );
-    if (response.rowCount === 0) {
-      return res.status(400).send({
-        error: "Leaderboard is empty",
+    if (seniors.includes(req.user.year)) {
+      const response = await client.query(
+        "select  users.first_name, users.last_name, MIN(dataquest2.public_accuracy), users.email from dataquest2 join users on dataquest2.fk_user = users.id where users.year = $1 or users.year = $2 group by users.first_name, users.last_name, users.email order by 3 limit 3",
+        isSenior
+      );
+      if (response.rowCount === 0) {
+        return res.status(400).send({
+          error: "Leaderboard is empty",
+        });
+      }
+      res.send({
+        submissions: response.rows,
+      });
+    } else {
+      const response = await client.query(
+        "select  users.first_name, users.last_name, MIN(dataquest2.public_accuracy), users.email from dataquest2 join users on dataquest2.fk_user = users.id where users.year = $1 or users.year = $2 group by users.first_name, users.last_name, users.email order by 3 limit 3",
+        isSenior
+      );
+      if (response.rowCount === 0) {
+        return res.status(400).send({
+          error: "Leaderboard is empty",
+        });
+      }
+      res.send({
+        submissions: response.rows,
       });
     }
-    res.send({
-      submissions: response.rows,
-    });
   } catch (error) {
     console.log(error);
     res.status(500).send({
