b93cc89763ab92649bfd5969b0301c8d3e18bd02
PranavJaju <pranavjaju45@gmail.com>
2023-10-16 20:44:21 +0530

    Changes for Tech or Treat

--- server/models/master.js
+++ server/models/master.js
@@ -1,43 +1,33 @@
 const masterEvents = [
   {
     id: 1,
-    ems_id: 6,
+    ems_id: 4,
     name: "Web N App",
   },
   {
     id: 2,
-    ems_id: 15,
+    ems_id: 12,
     name: "Insight",
   },
   {
     id: 3,
-    ems_id: 16,
-    name: "Photoshop Royale",
+    ems_id: 13,
+    name: "Freeze the Second",
   },
   {
     id: 4,
-    ems_id: 7,
+    ems_id: 5,
     name: "DataQuest",
   },
-  {
-    id: 5,
-    ems_id: 14,
-    name: "Paper Presentation",
-  },
-  {
-    id: 6,
-    ems_id: 18,
-    name: "Freeze the second",
-  },
+ 
+ 
 ];
 
 const emsIds = {
-  dataquest: 7,
-  web: 6,
-  insight: 15,
-  photoshop: 16,
-  paper: 14,
-  freeze: 18,
+  dataquest: 5,
+  web: 4,
+  insight: 12,
+  freeze: 13,
 };
 
 module.exports = { masterEvents, emsIds };
